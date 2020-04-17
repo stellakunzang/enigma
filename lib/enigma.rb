3fd073170685b016ba4da48404613c9ac81d6666
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-17 17:03:57 -0600

    Add skeleton code for encrypt and decrypt methods

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,5 +2,21 @@ class Enigma
 
   def initialize
   end
-  
+
+  def encrypt(text, key = random, date = today)
+    data = Hash.new
+    data[:encryption] = scrambled_text
+    data[:key] = key
+    data[:date] = date
+    return data
+  end
+
+  def decrypt(text, key, date = today)
+    data = Hash.new
+    data[:encryption] = unscrambled_text
+    data[:key] = key
+    data[:date] = date
+    return data
+  end
+
 end

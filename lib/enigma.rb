6899be89fbd5d8cfa4e01aefa87149bb5676304a
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-18 13:37:21 -0600

    Implement defaultable module

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,15 +1,29 @@
+require_relative "defaultable"
+
 class Enigma
 
+  include Defaultable
+
   def initialize
+    @encrypt_message = nil
+    @encrypt_key = nil
+    @encrypt_date = nil
   end
 
-  def encrypt(text, key = nil, date = nil)
-    shift = Shift.new(text, key, date)
+  def encrypt(text, key = randomize, date = today)
+    create_shift(text, key, date)
     data = Hash.new
-    data[:encryption] = shift.encrypt_message
-    data[:key] = shift.keys.join
-    data[:date] = shift.offsets.join
-    binding.pry
+    data[:encryption] = @encrypt_message
+    data[:key] = @encrypt_key
+    data[:date] = @encrypt_date
+    data
+  end
+
+  def create_shift(text, key, date)
+    shift = Shift.new(text, key, date)
+    @encrypt_message = shift.encrypt_message
+    @encrypt_key = shift.key
+    @encrypt_date = shift.date
   end
 
   def decrypt(text, key, date = today)

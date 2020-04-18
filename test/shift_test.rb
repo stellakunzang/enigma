a45d02b0164d4000302f9b6c595c9aaf181acd5b
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-17 21:03:07 -0600

    Split tests for offsets and keys

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -11,14 +11,37 @@ require "mocha/minitest"
 class ShiftTest < Minitest::Test
 
   def test_it_exists
-    shift = Shift.new
+    shift = Shift.new("Hello World", "02715", "040895")
     assert_instance_of Shift, shift
   end
 
   def test_it_has_attributes
     shift = Shift.new("Hello World", "02715", "040895")
-    assert_equal [02, 27, 71, 15],  shift.keys
-    assert_equal [1, 0, 2, 5], shift.offsets
+    assert_equal "Hello World", shift.text
+    assert_equal "02715", shift.key
+    assert_equal "040895", shift.date
+  end
+
+  def test_it_can_get_keys
+    shift = Shift.new("Hello World", "02715", "040895")
+    assert_equal [02, 27, 71, 15],  shift.get_keys("02715")
+  end
+
+  def test_it_can_get_keys_no_input
+    shift = Shift.new("Hello World")
+    
+  end
+
+  def test_it_can_get_offsets
+    shift = Shift.new("Hello World", "02715", "040895")
+    assert_equal [1, 0, 2, 5], shift.get_offsets("040895")
+  end
+
+  def test_it_can_get_offsets_no_input
+
+  end
+
+  def test_it_can_get_shifts
     assert_equal [3, 27, 73, 20], shift.shifts
   end
 

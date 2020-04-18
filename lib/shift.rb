9473e4815a5124b7b1b342c35fb49d43e633b63c
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-17 21:08:36 -0600

    Add alphabet to initialize

--- lib/shift.rb
+++ lib/shift.rb
@@ -6,6 +6,7 @@ class Shift
     @text = text
     @key = key
     @date = date
+    @alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
   end
 
   def get_keys(key)
@@ -24,10 +25,6 @@ class Shift
       offset = Offset.new(date)
     end
     offset.offsets
-  end
-
-  def alphabet
-    ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
   end
 
 end

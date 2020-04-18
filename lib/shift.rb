d04a82c3533bba6c3ede0c6efa2e7ca5458ee117
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-17 21:12:23 -0600

    Add keys and offsets instance variables

--- lib/shift.rb
+++ lib/shift.rb
@@ -1,12 +1,15 @@
 class Shift
 
-  attr_reader :text, :key, :date
+  attr_reader :text, :key, :date, :alphabet, :keys, :offsets
 
   def initialize(text, key = nil, date = nil)
     @text = text
     @key = key
     @date = date
     @alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+    @keys = nil
+    @offsets = nil
+
   end
 
   def get_keys(key)
@@ -15,7 +18,7 @@ class Shift
     else
       cypher_key = Key.new(key)
     end
-    cypher_key.keys
+    @keys = cypher_key.keys
   end
 
   def get_offsets(date)
@@ -24,7 +27,11 @@ class Shift
     else
       offset = Offset.new(date)
     end
-    offset.offsets
+    @offsets = offset.offsets
   end
+
+  def get_shifts
+  end
+
 
 end

660fb76174e21ec0bfbb94700977dc3152278b44
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-18 16:41:31 -0600

    Move methods common to shift and unshift into module

--- lib/defaultable.rb
+++ lib/defaultable.rb
@@ -8,4 +8,17 @@ module Defaultable
     (Date.today.strftime("%d%m%y")).to_s
   end
 
+  def square_date(date)
+    date = date.to_i
+    date * date
+  end
+
+  def alphabet_index
+    alphabet_with_index = {}
+    @alphabet.to_enum.with_index do |letter, index|
+      alphabet_with_index[letter] = index
+    end
+    alphabet_with_index
+  end
+
 end

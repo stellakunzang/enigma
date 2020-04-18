c9e382245f3dbf02ce9c2a67404ce7d3c0f833e0
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-17 20:32:52 -0600

    Add methods to generate keys and pass to other class

--- lib/key.rb
+++ lib/key.rb
@@ -8,5 +8,25 @@ class Key
   def randomize
     rand(5 ** 5).to_s.rjust(5, '0')
   end
+
+  def a
+    number[0..1].to_i
+  end
+
+  def b
+    number[1..2].to_i
+  end
+
+  def c
+    number[2..3].to_i
+  end
+
+  def d
+    number[3..4].to_i
+  end
+
+  def keys
+    [a, b, c, d]
+  end
   
 end

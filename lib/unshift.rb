3ca2cf34a6e7b5f7a86ccd7141b0ed7bc4be8b50
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-18 17:00:51 -0600

    Add abcdable module

--- lib/unshift.rb
+++ lib/unshift.rb
@@ -1,8 +1,10 @@
 require_relative "defaultable"
+require_relative "abcdable"
 
 class Unshift
 
   include Defaultable
+  include Abcdable
 
   attr_reader :text, :key, :date, :date_squared, :last_four, :alphabet
 
@@ -14,7 +16,12 @@ class Unshift
     @last_four = @date_squared[-4..-1]
     @alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
     @alphabet_index ||= alphabet_index
+    @unshifts = get_shifts
   end
 
+
+
+# need to use same keys and offsets to shift in the opposite direction
+# this can be accomplished by calling rotate with the same number as shift, but adding a "-" to the front of each
 
 end

7ecb5905a5e7fbabeef1bccad085a66b31ffc9f5
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-18 16:43:27 -0600

    Include defaultable, initialize instance variables

--- lib/unshift.rb
+++ lib/unshift.rb
@@ -1,5 +1,20 @@
-class Unshift
-  def initialize
+require_relative "defaultable"
 
-  end 
+class Unshift
+
+  include Defaultable
+
+  attr_reader :text, :key, :date, :date_squared, :last_four, :alphabet
+
+  def initialize(text, key, date = today)
+    @text = text
+    @key = key
+    @date = date
+    @date_squared ||= square_date(date).to_s
+    @last_four = @date_squared[-4..-1]
+    @alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+    @alphabet_index ||= alphabet_index
+  end
+
+
 end

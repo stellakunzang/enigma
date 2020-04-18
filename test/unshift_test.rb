1a6bac912c76089081c4f846cb9afc3f20cfba42
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-18 17:01:05 -0600

    Add test for keys and offsets

--- test/unshift_test.rb
+++ test/unshift_test.rb
@@ -23,10 +23,10 @@ class UnshiftTest < Minitest::Test
     assert_equal ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "], unshift.alphabet
   end
 
-  # def test_it_inherits_keys_offsets
-  #   unshift = Unshift.new("keder ohulw", "02715", "040895")
-  #   assert_equal [02, 27, 71, 15], unshift.keys
-  #   assert_equal [1, 0, 2, 5], unshift.offsets
-  # end
+  def test_it_can_get_keys_offsets
+    unshift = Unshift.new("keder ohulw", "02715", "040895")
+    assert_equal [02, 27, 71, 15], unshift.keys
+    assert_equal [1, 0, 2, 5], unshift.offsets
+  end
 
 end

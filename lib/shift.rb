ca3c9ef7e7506b6e7089c25fd727f6b38eca8d84
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-17 22:31:53 -0600

    Add encrypt message method

--- lib/shift.rb
+++ lib/shift.rb
@@ -7,10 +7,18 @@ class Shift
     @key = key
     @date = date
     @alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+    @alphabet_index ||= alphabet_index
     @keys = nil
     @offsets = nil
     @shifts = nil
+  end
 
+  def alphabet_index
+    alphabet_with_index = {}
+    @alphabet.to_enum.with_index do |letter, index|
+      alphabet_with_index[letter] = index
+    end
+    alphabet_with_index
   end
 
   def get_keys(key)
@@ -38,5 +46,44 @@ class Shift
     end
   end
 
+  def encrypt_message(text)
+    shift_a = @shifts[0]
+    shift_b = @shifts[1]
+    shift_c = @shifts[2]
+    shift_d = @shifts[3]
+    index_a = 0
+    index_b = 1
+    index_c = 2
+    index_d = 3
+
+    alphabet_a = alphabet.rotate(shift_a)
+    alphabet_b = alphabet.rotate(shift_b)
+    alphabet_c = alphabet.rotate(shift_c)
+    alphabet_d = alphabet.rotate(shift_d)
+
+    encrypted_message = []
+
+    raw_text = text.downcase.split("").to_enum
+    raw_text.with_index do |letter, index|
+      if index == index_a
+        new_letter = alphabet_a[@alphabet_index[letter]]
+        encrypted_message << new_letter
+        index_a += 4
+      elsif index == index_b
+        new_letter = alphabet_b[@alphabet_index[letter]]
+        encrypted_message << new_letter
+        index_b += 4
+      elsif index == index_c
+        new_letter = alphabet_c[@alphabet_index[letter]]
+        encrypted_message << new_letter
+        index_c += 4
+      elsif index == index_d
+        new_letter = alphabet_d[@alphabet_index[letter]]
+        encrypted_message << new_letter
+        index_d += 4
+      end
+    end
+    encrypted_message.join
+  end
 
 end

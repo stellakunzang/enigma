464f4d5a479f4894ce66d342746cb74918c8e16f
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-17 20:42:04 -0600

    Add test for attributes

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -14,4 +14,12 @@ class ShiftTest < Minitest::Test
     shift = Shift.new
     assert_instance_of Shift, shift
   end
-end 
+
+  def test_it_has_attributes
+    shift = Shift.new("Hello World", "02715", "040895")
+    assert_equal [02, 27, 71, 15],  shift.keys
+    assert_equal [1, 0, 2, 5], shift.offsets
+    assert_equal [3, 27, 73, 20], shift.shifts
+  end
+
+end

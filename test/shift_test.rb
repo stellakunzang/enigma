5d3dfb5b3ab540d9a4edff3358651fe067b442b9
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-18 22:04:33 -0600

    Moved unshift tests to shift

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -24,7 +24,7 @@ class ShiftTest < Minitest::Test
 
   def test_it_can_randomize_key
     shift = Shift.new("Hello World")
-    assert_equal true, shift.key.length  == 5
+    assert_equal true, shift.key.length == 5
   end
 
   def test_it_can_get_keys
@@ -75,14 +75,24 @@ class ShiftTest < Minitest::Test
     assert_equal ({ :a => 3, :b => 27, :c => 73, :d => 20}), shift.shifts_pairs
   end
 
+  def test_it_can_make_negative_shift_hash
+    shift = Shift.new("keder ohulw", "02715", "040895")
+    assert_equal ({ :a => -3, :b => -27, :c => -73, :d => -20}), shift.neg_shifts_pairs
+  end
+
   def test_it_can_generate_hash_with_starting_indexes_for_encrypt_method
     shift = Shift.new("Hello World", "02715", "040895")
-    assert_equal ({ :a => 0, :b => 1, :c => 2, :d => 3}), shift.indexes 
+    assert_equal ({ :a => 0, :b => 1, :c => 2, :d => 3}), shift.indexes
   end
 
   def test_it_can_encrypt_text
     shift = Shift.new("Hello World", "02715", "040895")
     assert_equal "keder ohulw", shift.encrypt_message
+  end
+
+  def test_it_can_decrypt_text
+    shift = Shift.new("keder ohulw", "02715", "040895")
+    assert_equal "hello world", shift.decrypt_message
   end
 
   def test_it_can_encrypt_text_with_punctuation

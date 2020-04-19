02c94fbe9c5f23d5584e720e1d0b1b3e56aa31f7
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-19 15:28:46 -0600

    Add test for reverse method

--- test/decrypt_test.rb
+++ test/decrypt_test.rb
@@ -109,6 +109,10 @@ class DecryptTest < Minitest::Test
     assert_equal ({"a"=>0,"b"=>1,"c"=>2,"d"=>3,"e"=>4,"f"=>5,"g"=>6,"h"=>7,"i"=>8,"j"=>9,"k"=>10,"l"=>11,"m"=>12,"n"=>13,"o"=>14,"p"=>15,"q"=>16,"r"=>17,"s"=>18,"t"=>19,"u"=>20,"v"=>21,"w"=>22,"x"=>23,"y"=>24,"z"=>25," "=>26}), decrypt.alphabet_index
   end
 
-  # def test_it_can_reverse
-  # end
+  def test_it_can_reverse
+    decrypt = Decrypt.new("Hello World", "02715", "040895")
+    decrypt.reverse("a", "f")
+    assert_equal "c", decrypt.decrypted_message
+    assert_equal ({:a => 4, :b => 1, :c => 2, :d => 3}), decrypt.indexes
+  end
 end

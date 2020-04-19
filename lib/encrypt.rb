679da74242e79f43f552ce428d07dc6fe15c780f
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-19 14:39:29 -0600

    Refactor as encrypt child class of shift parent class"

---
+++ lib/encrypt.rb
@@ -0,0 +1,55 @@
+require_relative "abcdable"
+require_relative "defaultable"
+require_relative "shift"
+
+class Encrypt < Shift
+
+  include Abcdable
+  include Defaultable
+
+  attr_reader :indexes,
+              :encrypted_message
+
+  def initialize(text, key = randomize, date = today)
+    @text = super
+    @key = super
+    @date = super
+    @shifts = super
+    @alphabet_index = alphabet_index
+    @indexes = set_indexes
+    @encrypted_message = [].join
+  end
+
+  def turn(position, letter)
+    local_alphabet = alphabet.rotate(shifts_pairs[position.to_sym])
+    new_letter = local_alphabet[@alphabet_index[letter]]
+    @encrypted_message << new_letter
+    @indexes[position.to_sym] += 4
+  end
+
+  # can I remove alphabet, indexes, etc. from attr_reader and instance variables and just call the methods?
+
+  def encrypt(index, letter)
+    if index == @indexes[:a]
+      turn("a", letter)
+    elsif index == @indexes[:b]
+      turn("b", letter)
+    elsif index == @indexes[:c]
+      turn("c", letter)
+    elsif index == @indexes[:d]
+      turn("d", letter)
+    end
+  end
+
+  def encrypt_message
+    raw_text = @text.split("").to_enum
+    raw_text.with_index do |letter, index|
+      if !alphabet.include?(letter)
+        @encrypted_message << letter
+      else
+        encrypt(index, letter)
+      end
+    end
+  end
+
+end

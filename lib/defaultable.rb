0665e57f203f28422d3150ed98a649a00c59fea3
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-18 20:10:13 -0600

    Remove alphabet methods

--- lib/defaultable.rb
+++ lib/defaultable.rb
@@ -14,16 +14,5 @@ module Defaultable
     date * date
   end
 
-  def alphabet
-    ("a".."z").to_a << " "
-  end
-
-  def alphabet_index
-    alphabet_with_index = {}
-    alphabet.to_enum.with_index do |letter, index|
-      alphabet_with_index[letter] = index
-    end
-    alphabet_with_index
-  end
 
 end

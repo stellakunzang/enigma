f2d6dc665942186b34fd0919c0b91045701366ee
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-18 18:46:56 -0600

    Create file that takes CLI and returns encrypted text file

---
+++ encrypt.rb
@@ -0,0 +1,23 @@
+require "./lib/enigma"
+require "pry"
+
+enigma = Enigma.new
+
+text = File.open(ARGV[0], "r")
+
+if ARGV[3]
+  enigma.encrypt(text.read, ARGV[2], ARGV[3])
+elsif ARGV[2]
+  enigma.encrypt(text.read, ARGV[2])
+else
+  enigma.encrypt(text.read)
+end
+
+text.close
+encrypted_text = enigma.encrypt_message
+
+encrypted = File.open(ARGV[1], "w")
+encrypted.write(encrypted_text)
+encrypted.close
+
+puts "Created #{ARGV[1]} with the key #{enigma.encrypt_key} and the date #{enigma.encrypt_date}"

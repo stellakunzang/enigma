bfa003c5978678b809e47b594f70305247dbd5b3
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-19 14:39:51 -0600

    Move methods into child classes

--- lib/abcdable.rb
+++ lib/abcdable.rb
@@ -12,49 +12,9 @@ module Abcdable
     alphabet_with_index
   end
 
-  def offsets
-    offsets = @last_four.split("")
-    offsets.map {|number| number.to_i}
-  end
-
-  def a
-    @key[0..1].to_i
-  end
-
-  def b
-    @key[1..2].to_i
-  end
-
-  def c
-    @key[2..3].to_i
-  end
-
-  def d
-    @key[3..4].to_i
-  end
-
-  def keys
-    [a, b, c, d]
-  end
-
-  def get_shifts
-    key_offset_pairs = keys.zip(offsets)
-    @shifts = key_offset_pairs.map do |pair|
-      pair.sum
-    end
-  end
-
   def shifts_pairs
     letters = [:a, :b, :c, :d]
-    Hash[letters.zip(@shifts)]
-  end
-
-  def neg_shifts_pairs
-    negative_pairs = {}
-    shifts_pairs.map do |letter, digit|
-      negative_pairs[letter] = - digit
-    end
-    negative_pairs
+    Hash[letters.zip(get_shifts)]
   end
 
   def set_indexes
@@ -62,5 +22,50 @@ module Abcdable
     starting_indexes = [0, 1, 2, 3]
     Hash[letters.zip(starting_indexes)]
   end
+
+  # def offsets
+  #   offsets = @last_four.split("")
+  #   offsets.map {|number| number.to_i}
+  # end
+  #
+  # def a
+  #   @key[0..1].to_i
+  # end
+  #
+  # def b
+  #   @key[1..2].to_i
+  # end
+  #
+  # def c
+  #   @key[2..3].to_i
+  # end
+  #
+  # def d
+  #   @key[3..4].to_i
+  # end
+  #
+  # def keys
+  #   [a, b, c, d]
+  # end
+  #
+  # def get_shifts
+  #   key_offset_pairs = keys.zip(offsets)
+  #   @shifts = key_offset_pairs.map do |pair|
+  #     pair.sum
+  #   end
+  # end
+  #
+  # def shifts_pairs
+  #   letters = [:a, :b, :c, :d]
+  #   Hash[letters.zip(@shifts)]
+  # end
+  #
+  # def neg_shifts_pairs
+  #   negative_pairs = {}
+  #   shifts_pairs.map do |letter, digit|
+  #     negative_pairs[letter] = - digit
+  #   end
+  #   negative_pairs
+  # end
 
 end
